055a07171deb7bf1b62fcb82b913fc0c943cbf1c
tejest <104548688+tejest@users.noreply.github.com>
2022-04-28 10:36:10 +0530

    Create q1.c

---
+++ q1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    float num1,num2;
+double resnum1,resnum2;
+scanf("%f",&num1);
+scanf("%f",&num2);
+scanf("%lf",&resnum1);
+scanf("%lf",&resnum2);
+printf("\n%lf",num1);
+printf("\n%lf",num2);
+	return 0;
+}
